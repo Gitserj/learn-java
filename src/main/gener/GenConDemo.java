49b779be835c0e01716ad0bc61455c73f6b3c3c6
serj.parshin <serj.parshin@gmail.com>
2019-02-26 20:28:09 +0500

    Пример обобщенного конструктора у необобщенного класса

---
+++ src/main/gener/GenConDemo.java
@@ -0,0 +1,21 @@
+package main.gener;
+
+class Summation {
+    private int sum;
+
+    <T extends Number> Summation(T num) {
+        sum = 0;
+        for(int i = 0; i <= num.intValue(); i++) sum += i;
+    }
+
+    int getSum() {
+        return sum;
+    }
+}
+
+public class GenConDemo {
+    public static void main(String[] args) {
+        Summation ob = new Summation(11.9);
+        System.out.println("Сумма целых чисел от 0 до 11.9: " + ob.getSum());
+    }
+}
