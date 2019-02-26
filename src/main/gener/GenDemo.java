939c26c37901d36b1846e8485fcd6a75feeff476
serj.parshin <serj.parshin@gmail.com>
2019-02-26 19:27:11 +0500

    Пример обобщений

---
+++ src/main/gener/GenDemo.java
@@ -0,0 +1,34 @@
+package main.gener;
+
+class Gen<T> {
+    T ob;
+
+    Gen(T o){
+        ob = o;
+    }
+
+    T getOb(){
+        return ob;
+    }
+
+    void showType(){
+        System.out.println("Тип Т - это " + ob.getClass().getName());
+    }
+}
+
+public class GenDemo {
+    public static void main(String[] args) {
+        Gen<Integer> iOb;
+        iOb = new Gen<Integer>(88);
+        iOb.showType();
+        int v = iOb.getOb();
+        System.out.printf("значение: %d\n\n", v);
+
+        Gen<String> sOb;
+        sOb = new Gen<String>("Тестирование строки");
+        sOb.showType();
+        String str = sOb.getOb();
+        System.out.printf("значение: %s\n\n", str);
+
+    }
+}
---
+++ src/main/gener/WildCardDemo.java
@@ -0,0 +1,40 @@
+package main.gener;
+
+class NumFns <T extends Number> {
+    T num;
+
+    NumFns(T n){
+        num = n;
+    }
+
+    double reciprocal() {
+        return 1/num.doubleValue();
+    }
+
+    double fraction() {
+        return num.doubleValue() - num.intValue();
+    }
+
+    boolean absEqual(NumFns<?> ob) {
+        if(Math.abs(num.doubleValue()) == Math.abs(ob.num.doubleValue())) return true;
+        return false;
+    }
+}
+
+public class WildCardDemo {
+    public static void main(String[] args) {
+        NumFns<Integer> obi = new NumFns<Integer>(6);
+        NumFns<Double> obd = new NumFns<Double>(-6.0);
+        NumFns<Double> obdd = new NumFns<Double>(6.9);
+        NumFns<Long> obl = new NumFns<Long>(5L);
+
+        System.out.println("Дробная часть " + obdd.num + " : " + obdd.fraction());
+        System.out.println("Обратная величина " + obdd.num + " : " + obdd.reciprocal());
+
+        if(obi.absEqual(obd)) System.out.println("Абсолютные значнения " + obi.num + " и " + obd.num + " совпадают");
+        else System.out.println("Абсолютные значнения " + obi.num + " и " + obd.num + " отличаются");
+
+        if(obi.absEqual(obl)) System.out.println("Абсолютные значнения " + obi.num + " и " + obl.num + " совпадают");
+        else System.out.println("Абсолютные значнения " + obi.num + " и " + obl.num + " отличаются");
+    }
+}
